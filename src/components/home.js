4881b65ec5605fdb443b5f8512956cd449951b4e
Paul Kum <kbpaul2016@gmail.com>
2024-08-06 12:06:17 +0400

    Development continuation

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,19 @@
 export default function home() {
-    const homeDiv = document.createElement('div');
-    const h1 = document.createElement('h1');
-    h1.textContent = "Welcome to G'sK Restaurant - Home";
-    homeDiv.appendChild(h1);
-    return homeDiv;
+    const component = document.createElement("div");
+    component.classList.add("main__container");
+
+    const header = document.createElement("h1");
+    header.textContent = "Welcome to G'sK";
+
+    component.appendChild(header);
+
+    const para = document.createElement("p");
+    para.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Varius sed pharetra dictum neque massa congue Lorem ipsum dolor sit amet, consectetur adipiscing elit. Varius sed pharetra dictum neque massa congue Lorem ipsum dolor sit amet, consectetur adipiscing elit. Varius sed pharetra dictum neque massa congue";
+    component.appendChild(para);
+
+    const button = document.createElement("button");
+    button.textContent = "Place an Order";
+    component.appendChild(button);
+
+    return component;
 }
